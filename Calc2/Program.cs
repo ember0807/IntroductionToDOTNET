36a462de22a33e06aae465e9717324f1986e48e8
XDavy_la41 <XDavy_la41@top-academy.ru>
2024-06-28 15:57:01 +0300

    рабочий калькулятор

--- Calc2/Program.cs
+++ Calc2/Program.cs
@@ -1,25 +1,27 @@
-﻿//#define TALK1
-#define TALK2
+﻿
+//#define CALC_1
+#define CALC_2
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calc2
+namespace Calc
 {
-	internal class Program
+	class Program
 	{
+		static string expression;
 		static void Main(string[] args)
 		{
-#if TALK1
-			Console.WriteLine("Введите просто арифметическое выражение : ");
+#if CALC_1
+			Console.Write("Введите простое арифметическое выражение: ");
 			string expression = Console.ReadLine();
-			expression = expression.Replace('.', ','); // изменяет одно значение на другое при вводе нужно сохранять в переменную
+			expression = expression.Replace(',', '.');
 			Console.WriteLine(expression);
 			String[] numbers = expression.Split('+', '-', '*', '/');
-			//for (int i = 0; i < numbers.Length; i++) Console.WriteLine(numbers[i]+"\t");			
+			//for (int i = 0; i < numbers.Length; i++)Console.Write(numbers[i] + "\t");
 			double a = Convert.ToDouble(numbers[0]);
 			double b = Convert.ToDouble(numbers[1]);
 			char s = expression[expression.IndexOfAny(new char[] { '+', '-', '*', '/' })];
@@ -27,27 +29,35 @@ namespace Calc2
 			Console.WriteLine(b);
 			Console.WriteLine(s);
 			switch (s)
-			{ 
-				case '+': Console.WriteLine($"{a} + {b} = {a + b}");break;
-				case '-': Console.WriteLine($"{a} - {b} = {a - b}");break;
-				case '*': Console.WriteLine($"{a} * {b} = {a * b}");break;
-				case '/': Console.WriteLine($"{a} / {b} = {a / b}");break;
-			
-			}
-			Main(args);// рукурсивный вызов функции main(), при завершении программы она заново запускается
+			{
+				case '+': Console.WriteLine($"{a} + {b} = {a + b}"); break;
+				case '-': Console.WriteLine($"{a} - {b} = {a - b}"); break;
+				case '*': Console.WriteLine($"{a} * {b} = {a * b}"); break;
+				case '/': Console.WriteLine($"{a} / {b} = {a / b}"); break;
+			} 
 #endif
-			//correct = true;
-			//Console.WriteLine("Введите арифмитическое выражение");
+			bool correct = true;
+			//do
+			//{
+			//Flat expression DONE
+			correct = true;
+			//Console.Write("Введите арифметические выражение: ");
 			//string expression = Console.ReadLine();
 			//string expression = "22 + 33 * 44 - 55 / 5";
-			string expression = "(22 + 33 *( 44 + 55)) / 5";
-			Console.WriteLine($"Result: {Browse(expression)}");
-			//Console.WriteLine("Введите арифмитическое выражение");
-			
+			//string expression = "22 + 33 + 44 + 55 - 5";
+			//string expression = "22 * 33 / 44 * 55 / 5";
+			//-------------------------------
+			expression = "(22 + 33 * (44 + 55)) / 5";
+			//expression = "(11 + (55+(22+33)/4-5)*2*3+3)*(3+2)";
+			Console.WriteLine(expression);
+			Console.WriteLine(Browse(expression));
+			Console.WriteLine(Calculate(expression));
+			//Console.WriteLine($"Result: {Calculate(Browse(expression))}");
+			//} while (!correct);
 
-            //Main(args);
-        }
-		static double Browse(string expression)
+			//Main(args);	//Рекурсивный вызов функции Main(), при завершении программы она заново звпускается.
+		}
+		static string Browse(string expression)
 		{
 			expression = expression.Replace(" ", "");
 			for (int i = 0; i < expression.Length; i++)
@@ -55,30 +65,45 @@ namespace Calc2
 				if (expression[i] == '(')
 				{
 					for (int j = i + 1; j < expression.Length; j++)
-					{ 
+					{
+						if (expression[j] == '(')
+						{
+							string buffer = expression.Substring(j + 1, expression.Length - j - 1);
+							Browse(buffer);
+						}
 						if (expression[j] == ')')
 						{
-							//string subexpression = expression.Substring(i, j-i);
+							//string subexpression = expression.Substring(i, j - i);
 							//double result = Calculate(subexpression.Substring(1, subexpression.Length - 2));
-							//expression = expression.Replace(subexpression, result.ToString());
-							string subexpression = expression.Substring(i+1, j-i-1);
-							double result = Calculate(subexpression);
-							
-							expression = expression.Replace($"({subexpression})",result.ToString());
-							break;
-						}
-						else if (expression[j] == '(')
-						{
-							Browse(expression.Substring(j+1 , expression.Length - 1 - j));
+							string buffer = expression.Substring(i + 1, j - i - 1);
+							if (!buffer.Contains('(') && !buffer.Contains(')'))
+							{
+								double result = Calculate(buffer);
+								Program.expression = expression.Replace($"({buffer})", result.ToString());
+							}
+							Browse(Program.expression);
+							//break;
 						}
 					}
 				}
+				if (expression[i] == ')')
+				{
+					string buffer = expression.Substring(0, i);
+					if (!buffer.Contains('(') && !buffer.Contains(')'))
+					{
+						double result = Calculate(buffer);
+						Program.expression = expression.Replace($"{buffer})", result.ToString());
+					}
+					Browse(Program.expression);
+				}
 			}
-			return Calculate(expression);
+			return expression;
 		}
 		static double Calculate(string expression)
 		{
-			
+			expression = expression.Replace(" ", "");
+			Console.WriteLine(expression);
+
 			string s_operations = "+-*/";
 			String[] s_numbers = expression.Split(s_operations.ToCharArray());
 			s_numbers = s_numbers.Where(item => item != "").ToArray();
@@ -86,56 +111,49 @@ namespace Calc2
 			for (int i = 0; i < s_numbers.Length; i++)
 			{
 				d_numbers[i] = Convert.ToDouble(s_numbers[i]);
-				Console.Write(d_numbers[i] + "\t ");
-
+				Console.Write(d_numbers[i] + "\t");
 			}
 			Console.WriteLine();
 			char[] c_operations = expression.Where(item => s_operations.Contains(item)).ToArray();
 			for (int i = 0; i < c_operations.Length; i++)
 			{
-				Console.WriteLine(c_operations[i] + "\t");
+				Console.Write(c_operations[i] + "\t");
 			}
 			Console.WriteLine();
 
-			if (d_numbers.Length != c_operations.Length + 1)
-			{
-				//Console.WriteLine("Выражение некорректно, повторите ввод...");
-				//continue;
-				throw new Exception("Expression is incorrect");
-			}
+			if (d_numbers.Length != c_operations.Length + 1) throw new Exception("Expression is incorrect");
 
-			//while(c_operations.Contains('*')|| c_operations.Contains ('/'))
-			{
-				for (int i = 0; i < c_operations.Length; i++)
-				{
 
-					while (c_operations[i] == '*' || c_operations[i] == '/')
-					{
-						if (c_operations[i] == '*') d_numbers[i] *= d_numbers[i + 1];
-						if (c_operations[i] == '/') d_numbers[i] /= d_numbers[i + 1];
-
-						for (int j = i + 1; j < d_numbers.Length - 1; j++) d_numbers[j] = d_numbers[j + 1];
-						for (int j = i; j < c_operations.Length - 1; j++) c_operations[j] = c_operations[j + 1];
-						d_numbers[d_numbers.Length - 1] = 0;
-						c_operations[c_operations.Length - 1] = '\0';
-					}
-				}
-			}
 			for (int i = 0; i < c_operations.Length; i++)
 			{
-
-				while (c_operations[i] == '+' || c_operations[i] == '-')
+				//if (c_operations[0] == '\0') break;
+				//if (c_operations[1] == '\0') i = 0;
+				while (c_operations[i] == '*' || c_operations[i] == '/')
 				{
-					if (c_operations[i] == '+') d_numbers[i] += d_numbers[i + 1];
-					if (c_operations[i] == '-') d_numbers[i] -= d_numbers[i + 1];
-
+					if (c_operations[i] == '*') d_numbers[i] *= d_numbers[i + 1];
+					if (c_operations[i] == '/') d_numbers[i] /= d_numbers[i + 1];
 					for (int j = i + 1; j < d_numbers.Length - 1; j++) d_numbers[j] = d_numbers[j + 1];
 					for (int j = i; j < c_operations.Length - 1; j++) c_operations[j] = c_operations[j + 1];
 					d_numbers[d_numbers.Length - 1] = 0;
 					c_operations[c_operations.Length - 1] = '\0';
 				}
-
 			}
+
+			for (int i = 0; i < c_operations.Length; i++)
+			{
+				//if (c_operations[0] == '\0') break;
+				//if (c_operations[1] == '\0')i = 0;
+				while (c_operations[i] == '+' || c_operations[i] == '-')
+				{
+					if (c_operations[i] == '+') d_numbers[i] += d_numbers[i + 1];
+					if (c_operations[i] == '-') d_numbers[i] -= d_numbers[i + 1];
+					for (int j = i + 1; j < d_numbers.Length - 1; j++) d_numbers[j] = d_numbers[j + 1];
+					for (int j = i; j < c_operations.Length - 1; j++) c_operations[j] = c_operations[j + 1];
+					d_numbers[d_numbers.Length - 1] = 0;
+					c_operations[c_operations.Length - 1] = '\0';
+				}
+			}
+
 			for (int i = 0; i < d_numbers.Length; i++)
 			{
 				Console.Write(d_numbers[i] + "\t");
@@ -146,6 +164,7 @@ namespace Calc2
 				Console.Write(c_operations[i] + "\t");
 			}
 			Console.WriteLine();
+
 			return d_numbers[0];
 		}
 	}
