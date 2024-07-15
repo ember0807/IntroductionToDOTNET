d841ad5a6a68486c07473684ee0748ea5fe7f70a
XDavy_la41 <XDavy_la41@top-academy.ru>
2024-07-15 10:01:38 +0300

    AbstractGeometry1

---
+++ methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine(Sum(2, 3));
+		}
+
+		static int Sum(int a, int b)
+		{ 
+			return a + b;
+		}
+	}
+}
